582b24eee29347f87bb4055e144e66be11beddef
Ishigure <Ishigure@localhost>
2010-08-23 00:39:54 +0200

    remove some local files

--- tmp/smarty_c/%%05^050^05049E47%%wowhead.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-08-05 19:31:30
-         compiled from wowhead.tpl */ ?>
-    <script type="text/javascript" src="<?php echo $this->_tpl_vars['tt_script']; ?>
-"></script>
--- tmp/smarty_c/%%1B^1B1^1B1F6954%%button.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2010-08-05 19:36:42
-         compiled from button.tpl */ ?>
-<div><a class="button" style="width:<?php echo $this->_tpl_vars['bwidth']; ?>
-px;" href="<?php echo $this->_tpl_vars['blink']; ?>
-"><?php echo $this->_tpl_vars['btext']; ?>
-</a></div>
